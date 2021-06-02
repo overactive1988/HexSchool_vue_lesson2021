432464e0183aacbe1c7da9ee9bcad735a51d90b0
bhw-lee <t.lee@bhworks.com>
2021-06-02 11:01:41 +0900

    props修正及びtest追加

--- week04/assets/js/vue_products2.js
+++ week04/assets/js/vue_products2.js
@@ -1,8 +1,8 @@
-import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.11/vue.esm-browser.js'
-import pagination from './pagination.js'
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.11/vue.esm-browser.js";
+import pagination from "./pagination.js";
 
-let productModal = null // 定義接近全域變數
-let delProductModal = null // 定義接近全域變數
+let productModal = null; // 定義接近全域變數
+let delProductModal = null; // 定義接近全域變數
 
 const app = createApp({
   data() {
@@ -11,17 +11,17 @@ const app = createApp({
       // 使用this.即可呼叫此處資料內容
       isNew: true,
       products: [], // 主要商品結構
-      allproductsNum: '', // 合計商品數量
+      allproductsNum: "", // 合計商品數量
       tempProduct: {
         // 稍後調整資料使用的準備結構
         imagesUrl: [],
       },
-      pagination:{},
-    }
+      pagination: {},
+    };
   },
   // 區域註冊
   components: {
-    pagination
+    pagination,
   },
   mounted() {
     // 生命週期 → 元素掛載完成後觸發
@@ -29,19 +29,19 @@ const app = createApp({
     // 取得 Token
     const token = document.cookie.replace(
       /(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/,
-      '$1'
-    )
+      "$1"
+    );
     // 將 Token 夾帶至請請求(request) 的表頭(headers) 設定只需要一次
-    axios.defaults.headers.common['Authorization'] = token
+    axios.defaults.headers.common["Authorization"] = token;
 
     // Bootstrap Modal實體掛載
-    productModal = new bootstrap.Modal(document.getElementById('productModal'))
+    productModal = new bootstrap.Modal(document.getElementById("productModal"));
     delProductModal = new bootstrap.Modal(
-      document.getElementById('delProductModal')
-    )
+      document.getElementById("delProductModal")
+    );
 
-    this.loginCheck()
-    this.getData()
+    this.loginCheck();
+    this.getData();
   },
   methods: {
     // 函式的集合
@@ -51,18 +51,19 @@ const app = createApp({
         .post(`${baseUrl}/api/user/check`)
         .then((res) => {
           if (res.data.success) {
-            console.log('已確認為使用者本人登入')
+            console.log("已確認為使用者本人登入");
           } else {
-            alert('您尚未登入，請重新登入')
-            window.location = './index.html'
+            alert("您尚未登入，請重新登入");
+            // window.location = './index.html'
           }
         })
         .catch((err) => {
-          console.log(err)
-        })
+          console.log(err);
+        });
     },
     // 取得產品列表
-    getData(num = 1) { // 參數預設值
+    getData(num = 1) {
+      // 參數預設值
       axios
         .get(`${baseUrl}/api/${apiPath}/admin/products?page=${num}`)
         .then((res) => {
@@ -70,16 +71,16 @@ const app = createApp({
             // this.products = res.data.products
             // this.pagination = res.data.pagination
             // 解構寫法
-            const { products, pagination} = res.data;
-            this.products = products
-            this.pagination = pagination
-            console.log(res.data)
-            this.getAllproducts() // 重新取得產品數量
+            const { products, pagination } = res.data;
+            this.products = products;
+            this.pagination = pagination;
+            console.log(res.data);
+            this.getAllproducts(); // 重新取得產品數量
           }
         })
         .catch((error) => {
-          console.log(error)
-        })
+          console.log(error);
+        });
     },
     // 刪除單筆商品
     deleteProduct() {
@@ -88,92 +89,91 @@ const app = createApp({
           `${baseUrl}/api/${apiPath}/admin/product/${this.tempProduct.id}`
         )
         .then((res) => {
-          delProductModal.hide() // 關掉 modal
-          alert(`已刪除${this.tempProduct.title}商品`)
-          this.getData()
+          delProductModal.hide(); // 關掉 modal
+          alert(`已刪除${this.tempProduct.title}商品`);
+          this.getData();
         })
         .catch((error) => {
-          console.log(error)
-        })
+          console.log(error);
+        });
     },
     // 開啟modal
     openModal(isNew, item) {
       // console.log(isNew, item)
       switch (isNew) {
-        case 'new':
+        case "new":
           // 先將預設物件內容清空
           this.tempProduct = {
             imagesUrl: [],
-          }
-          this.isNew = true
-          productModal.show()
-          break
-        case 'edit':
+          };
+          this.isNew = true;
+          productModal.show();
+          break;
+        case "edit":
           // 因為傳參考特性會連動到資料，因此將資料進行淺層複製
-          this.tempProduct = { ...item }
-          this.isNew = false
-          productModal.show()
-          break
-        case 'delete':
+          this.tempProduct = { ...item };
+          this.isNew = false;
+          productModal.show();
+          break;
+        case "delete":
           // 因為傳參考特性會連動到資料，因此將資料進行淺層複製
-          this.tempProduct = { ...item }
+          this.tempProduct = { ...item };
           // Modal需要拿到 title 和刪除按鈕時需要獲得 id
-          delProductModal.show()
-          break
+          delProductModal.show();
+          break;
         default:
-          break
+          break;
       }
     },
     // 新增陣列圖片
     createImages() {
-      this.tempProduct.imagesUrl = ['']
+      this.tempProduct.imagesUrl = [""];
     },
     // 於modal內按下確認按鈕時觸發
     updateProduct(tempProduct) {
       // 預設為 新增
-      let apiUrl = `${baseUrl}/api/${apiPath}/admin/product/`
-      let httpMethod = 'post'
+      let apiUrl = `${baseUrl}/api/${apiPath}/admin/product/`;
+      let httpMethod = "post";
       // 根據 isNew 來判斷要串接 post 或是 put API
       if (!this.isNew) {
         // 進入編輯狀態
-        apiUrl = `${baseUrl}/api/${apiPath}/admin/product/${tempProduct.id}`
-        httpMethod = 'put'
+        apiUrl = `${baseUrl}/api/${apiPath}/admin/product/${tempProduct.id}`;
+        httpMethod = "put";
       }
       axios[httpMethod](apiUrl, { data: tempProduct }) // post 或 put
         .then((res) => {
           if (res.data.success) {
-            productModal.hide() // 關掉 modal
-            this.getData() // 重整畫面
+            productModal.hide(); // 關掉 modal
+            this.getData(); // 重整畫面
           } else {
-            alert(res.data.message)
+            alert(res.data.message);
           }
         })
         .catch((error) => {
-          console.log(error)
-        })
+          console.log(error);
+        });
     },
     // 取得所有產品數量
     getAllproducts() {
       axios.get(`${baseUrl}/api/${apiPath}/admin/products/all`).then((res) => {
-        this.allproductsNum = Object.values(res.data.products) // 將回傳的物件轉換為陣列
-      })
+        this.allproductsNum = Object.values(res.data.products); // 將回傳的物件轉換為陣列
+      });
     },
   },
   created() {
     // 元件生成，必定會執行的項目
   },
-})
+});
 
 // 定義全域元件 須放置在 createApp 後方，mount之前
 // 商品 Modal
-app.component('productModal',{
+app.component("productModal", {
   props: {
     tempProduct: {
       type: Object, // 驗證型別
-      default(){
-        return {
-        }
-      }
+      default() {
+        return {};
+      },
     },
   },
   template: `<div
@@ -388,22 +388,21 @@ app.component('productModal',{
   </div>`,
   methods: {
     createImages() {
-      this.tempProduct.imagesUrl = ['']
+      this.tempProduct.imagesUrl = [""];
     },
-  }
-})
+  },
+});
 
 // 將 deleteModal x-template 元件化
-app.component('delProductModal',{
+app.component("delProductModal", {
   props: {
-    tempProduct: {
+    anyName: {
       type: Object,
-      default(){
-        return {
-        }
-      }
+      default() {
+        return {};
+      },
     },
   },
   template: "#delModal-template",
-})
-app.mount('#app')
+});
+app.mount("#app");
