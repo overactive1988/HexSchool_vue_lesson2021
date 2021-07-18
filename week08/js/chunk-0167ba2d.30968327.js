99054680595f4f17654560981d58fb8743be393f
bhw-lee <t.lee@bhworks.com>
2021-07-19 05:02:06 +0900

    圖片整理&首頁連結對應

--- week08/js/chunk-0167ba2d.30968327.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-0167ba2d"],{d0fa:function(e,c,t){e.exports=t.p+"img/index_bg.9f5febc0.jpg"},ec27:function(e,c,t){"use strict";t.r(c);var n=t("7a23"),s=t("d0fa"),a=t.n(s),i={class:"container content"},r=Object(n["h"])("h1",{class:"text-center"},"首頁",-1),d=Object(n["h"])("figure",{class:"text-center"},[Object(n["h"])("img",{class:"img-fluid",src:a.a,alt:""})],-1);function o(e,c){return Object(n["v"])(),Object(n["d"])("div",i,[r,d])}const b={};b.render=o;c["default"]=b}}]);
-//# sourceMappingURL=chunk-0167ba2d.30968327.js.map
